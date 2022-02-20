84a8990fe23338e10f719906ce4b94a2f1eabd20
Romans Gainanovs <gainanov@amazon.com>
2022-02-20 20:37:31 +0000

    lesson 2

--- src/main/java/ru/geekbrains/spring/gainanovmarket/controllers/ProductController.java
+++ src/main/java/ru/geekbrains/spring/gainanovmarket/controllers/ProductController.java
@@ -2,10 +2,13 @@ package ru.geekbrains.spring.gainanovmarket.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import ru.geekbrains.spring.gainanovmarket.dtos.ProductDto;
 import ru.geekbrains.spring.gainanovmarket.entities.Product;
+import ru.geekbrains.spring.gainanovmarket.exceptions.ResourceNotFoundException;
 import ru.geekbrains.spring.gainanovmarket.services.ProductService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -14,13 +17,18 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
-    public List<Product> findAll() {
-        return productService.findAll();
+    public List<ProductDto> findAll() {
+        return productService.findAll().stream().map(
+                p -> new ProductDto(p.getId(), p.getTitle(), p.getPrice())).collect(Collectors.toList()
+        );
     }
 
     @GetMapping("/{id}")
-    public Product findById(@PathVariable Long id) {
-        return productService.findById(id).get();
+    public ProductDto findById(@PathVariable Long id) {
+        Product p =  productService.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Product with id: " + id + " not found.")
+        );
+        return new ProductDto(p.getId(), p.getTitle(), p.getPrice());
     }
 
     @DeleteMapping("/{id}")
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,8 +1,10 @@
 create table products
 (
-    id      bigserial primary key,
-    title   varchar(255),
-    price   int
+    id          bigserial primary key,
+    title       varchar(255),
+    price       int,
+    created_at  timestamp default current_timestamp,
+    updated_at  timestamp default current_timestamp
 );
 
 insert into products (title, price) values
