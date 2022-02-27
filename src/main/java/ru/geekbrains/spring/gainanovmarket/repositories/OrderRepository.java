60f9a7705eda86663f2ca95aa275d656d596551a
Romans Gainanovs <gainanov@amazon.com>
2022-02-27 00:12:57 +0000

    classwork

--- src/main/java/ru/geekbrains/spring/gainanovmarket/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.spring.gainanovmarket.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.geekbrains.spring.gainanovmarket.entities.Order;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- src/main/java/ru/geekbrains/spring/gainanovmarket/services/OrderService.java
+++ src/main/java/ru/geekbrains/spring/gainanovmarket/services/OrderService.java
@@ -1,44 +1,12 @@
 package ru.geekbrains.spring.gainanovmarket.services;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.geekbrains.spring.gainanovmarket.entities.Order;
-import ru.geekbrains.spring.gainanovmarket.entities.OrderItem;
 import ru.geekbrains.spring.gainanovmarket.entities.User;
-import ru.geekbrains.spring.gainanovmarket.model.Cart;
 import ru.geekbrains.spring.gainanovmarket.model.OrderData;
-import ru.geekbrains.spring.gainanovmarket.repositories.OrderItemRepository;
-import ru.geekbrains.spring.gainanovmarket.repositories.OrderRepository;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
 public class OrderService {
-    private final CartService cartService;
-    private final ProductService productService;
-    private final OrderRepository orderRepository;
-    private final OrderItemRepository orderItemRepository;
+    public void createOrder(User user, OrderData orderData){
 
-    public void createOrder(User user, OrderData orderData) {
-        Cart cart = cartService.getCurrentCart();
-        Order order = new Order();
-        order.setUser(user);
-        order.setAddress(orderData.getAddress());
-        order.setPhone(orderData.getPhone());
-        order.setTotalPrice(cart.getTotalPrice());
-        List<OrderItem> orderItemList = cart.getItems().stream().map(cartItem -> {
-            OrderItem oi = new OrderItem();
-            oi.setProduct(productService.findById(cartItem.getProductId()).get());
-            oi.setOrder(order);
-            oi.setPricePerProduct(cartItem.getPricePerProduct());
-            oi.setPrice(cartItem.getPrice());
-            oi.setQuantity(cartItem.getQuantity());
-            return oi;
-        }).collect(Collectors.toList());
-        orderRepository.saveAndFlush(order);
-        order.setItems(orderItemList);
-        orderItemRepository.saveAllAndFlush(orderItemList);
     }
 }
