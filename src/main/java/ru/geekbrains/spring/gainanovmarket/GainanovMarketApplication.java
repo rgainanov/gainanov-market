8118d1a48f8d8cd49e1cb46033ac315aaaf3b209
Romans Gainanovs <gainanov@amazon.com>
2022-03-08 15:21:14 +0000

    placeholder for homework

--- src/main/java/ru/geekbrains/spring/gainanovmarket/GainanovMarketApplication.java
+++ src/main/java/ru/geekbrains/spring/gainanovmarket/GainanovMarketApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GainanovMarketApplication {
 
+    // TODO Spring Cloud Homework 1
+    // TODO Spring Cloud Homework 2
     public static void main(String[] args) {
         SpringApplication.run(GainanovMarketApplication.class, args);
     }
-
 }
